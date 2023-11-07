cab2683b4d7e3c31bf0587ecc73d58a51fb07fce
rodrigoftw <rodrigoftw2@gmail.com>
2023-11-07 12:07:41 -0300

    Create Home Screen

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#1A1A1A',
+  },
+  mainContent: {
+    flex: 1,
+    width: '100%',
+    paddingHorizontal: 24
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    marginTop: -27,
+  }
+});
